88910010037afec60e75a3547d5240e2aeb19d2a
bevicted <170766675+bevicted@users.noreply.github.com>
2024-06-11 21:43:37 +0200

    twoSumV2
    Signed-off-by: bevicted <170766675+bevicted@users.noreply.github.com>

--- go/leetcode/1_two_sum.go
+++ go/leetcode/1_two_sum.go
@@ -1,5 +1,15 @@
 package main
 
+// 1. Two Sum
+//
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+type twoSumF func(nums []int, target int) []int
+
+// original submission
+// BenchmarkTwoSum-8       12280203                91.72 ns/op
 func twoSum(nums []int, target int) []int {
 	for i := 0; i < len(nums); i++ {
 		for j := i + 1; j < len(nums); j++ {
@@ -9,5 +19,21 @@ func twoSum(nums []int, target int) []int {
 		}
 	}
 
-	return []int{}
+	return nil
+}
+
+// performance + readability
+// based on the editorial and other submissions
+// local benchmarking seems to disagree though
+// BenchmarkTwoSumV2-8      1964156               644.5 ns/op
+func twoSumV2(nums []int, target int) []int {
+	complements := make(map[int]int, len(nums))
+	for i, num := range nums {
+		if j, ok := complements[target-num]; ok {
+			return []int{j, i}
+		}
+		complements[num] = i
+	}
+
+	return nil
 }
--- go/leetcode/1_two_sum_test.go
+++ go/leetcode/1_two_sum_test.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+)
+
+const (
+	benchmarkNumsLen = 10
+	benchmarkTarget  = 6
 )
 
 func TestTwoSum(t *testing.T) {
@@ -37,7 +43,9 @@ func TestTwoSum(t *testing.T) {
 		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
-			assert.Equal(t, tc.expectIndices, twoSum(tc.nums, tc.target))
+			for _, f := range []twoSumF{twoSum, twoSumV2} {
+				assert.Equal(t, tc.expectIndices, f(tc.nums, tc.target))
+			}
 		})
 	}
 }
@@ -51,12 +59,34 @@ func FuzzTwoSum(f *testing.F) {
 			return
 		}
 
-		assert.Empty(t, indices)
+		assert.Nil(t, indices)
 	})
+}
+
+func makeRandIntSlice(n int) []int {
+	s := make([]int, n)
+	for i := range n {
+		s[i] = rand.Intn(100)
+	}
+	return s
 }
 
 func BenchmarkTwoSum(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		twoSum([]int{3, 2, 4}, 6)
+		b.StopTimer()
+		nums := makeRandIntSlice(benchmarkNumsLen)
+
+		b.StartTimer()
+		twoSum(nums, benchmarkTarget)
+	}
+}
+
+func BenchmarkTwoSumV2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		nums := makeRandIntSlice(benchmarkNumsLen)
+
+		b.StartTimer()
+		twoSumV2(nums, benchmarkTarget)
 	}
 }
