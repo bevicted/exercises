4767e1fa29931a300551d5fcf579f8f0a74e75c5
bevicted <170766675+bevicted@users.noreply.github.com>
2024-06-11 16:11:16 +0200

    go/two sum
    Signed-off-by: bevicted <170766675+bevicted@users.noreply.github.com>

--- go/leetcode/1_two_sum.go
+++ go/leetcode/1_two_sum.go
@@ -1,5 +1,13 @@
 package main
 
 func twoSum(nums []int, target int) []int {
-	return []int{0}
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+
+	return []int{}
 }
--- go/leetcode/1_two_sum_test.go
+++ go/leetcode/1_two_sum_test.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTwoSum(t *testing.T) {
+	t.Parallel()
+
+	for _, tc := range []struct {
+		name          string
+		nums          []int
+		target        int
+		expectIndices []int
+	}{
+		{
+			name:          "example 1",
+			nums:          []int{2, 7, 11, 15},
+			target:        9,
+			expectIndices: []int{0, 1},
+		},
+		{
+			name:          "example 2",
+			nums:          []int{3, 2, 4},
+			target:        6,
+			expectIndices: []int{1, 2},
+		},
+		{
+			name:          "example 3",
+			nums:          []int{3, 3},
+			target:        6,
+			expectIndices: []int{0, 1},
+		},
+	} {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			assert.Equal(t, tc.expectIndices, twoSum(tc.nums, tc.target))
+		})
+	}
+}
