3a372c72c1e455ff131ecd7da144f6aaf446872b
bevicted <170766675+bevicted@users.noreply.github.com>
2025-06-04 13:11:54 +0200

    WIP: aoc2024-1
    Signed-off-by: bevicted <170766675+bevicted@users.noreply.github.com>

--- zig/adventofcode/2024/day_1.zig
+++ zig/adventofcode/2024/day_1.zig
@@ -4,32 +4,44 @@ const Allocator = std.mem.Allocator;
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}).init;
     defer {
-        const check = gpa.deinit();
-        if (check == .leak) @panic("memory leak detected");
+        if (gpa.deinit() == .leak) @panic("memory leak detected");
     }
     const malloc = gpa.allocator();
-    var a = [_]u8{ 3, 4, 2, 1, 3, 3 };
-    var b = [_]u8{ 4, 3, 5, 3, 9, 3 };
+    var a = [_]u32{ 3, 4, 2, 1, 3, 3 };
+    var b = [_]u32{ 4, 3, 5, 3, 9, 3 };
     const distance = try compareLists(malloc, &a, &b);
     std.debug.print("dist: {}", .{distance});
 }
 
 /// Creates sorted copies of the two slices and returns their distance.
-fn compareLists(malloc: Allocator, a: []const u8, b: []u8) Allocator.Error!u8 {
+fn compareLists(malloc: Allocator, a: []const u32, b: []const u32) Allocator.Error!u32 {
     const aSorted = try sortCopy(malloc, a);
     defer malloc.free(aSorted);
-    _ = b;
-    //const b_sorted = bsort(b);
+    const bSorted = try sortCopy(malloc, b);
+    defer malloc.free(bSorted);
 
-    std.debug.print("a sorted: {any}\n", .{aSorted});
-    // std.debug.print("b: {u}", .{b_sorted});
-    return 5;
+    var diff: u32 = 0;
+
+    for (0..a.len) |i| {
+        var bigger: u32 = undefined;
+        var smaller: u32 = undefined;
+        if (aSorted[i] > bSorted[i]) {
+            bigger = aSorted[i];
+            smaller = bSorted[i];
+        } else {
+            bigger = bSorted[i];
+            smaller = aSorted[i];
+        }
+        diff += bigger - smaller;
+    }
+
+    return diff;
 }
 
 /// Creates sorted copy of s.
 /// Caller owns the memory.
-fn sortCopy(malloc: Allocator, s: []const u8) Allocator.Error![]u8 {
-    const copy = try malloc.dupe(u8, s);
-    std.mem.sort(u8, copy, {}, std.sort.asc(u8));
+fn sortCopy(malloc: Allocator, s: []const u32) Allocator.Error![]u32 {
+    const copy = try malloc.dupe(u32, s);
+    std.mem.sort(u32, copy, {}, std.sort.asc(u32));
     return copy;
 }
