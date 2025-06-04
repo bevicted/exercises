10e40624623bc604a60f73f3a27071b9bd2aec83
bevicted <170766675+bevicted@users.noreply.github.com>
2025-06-04 10:29:35 +0200

    fix aoc1
    Signed-off-by: bevicted <170766675+bevicted@users.noreply.github.com>

--- zig/adventofcode/2024/day_1.zig
+++ zig/adventofcode/2024/day_1.zig
@@ -1,31 +1,35 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 
 pub fn main() !void {
-    const a = [_]u8{ 3, 4, 2, 1, 3, 3 };
-    const b = [_]u8{ 4, 3, 5, 3, 9, 3 };
-    const distance = try compareLists(a[0..], b[0..]);
+    var gpa = std.heap.GeneralPurposeAllocator(.{}).init;
+    defer {
+        const check = gpa.deinit();
+        if (check == .leak) @panic("memory leak detected");
+    }
+    const malloc = gpa.allocator();
+    var a = [_]u8{ 3, 4, 2, 1, 3, 3 };
+    var b = [_]u8{ 4, 3, 5, 3, 9, 3 };
+    const distance = try compareLists(malloc, &a, &b);
     std.debug.print("dist: {}", .{distance});
 }
 
-/// Returns the distance of two slices
-fn compareLists(a: []const u8, b: []const u8) std.mem.Allocator.Error!u8 {
-    const a_sorted = try bsort(a);
+/// Creates sorted copies of the two slices and returns their distance.
+fn compareLists(malloc: Allocator, a: []const u8, b: []u8) Allocator.Error!u8 {
+    const aSorted = try sortCopy(malloc, a);
+    defer malloc.free(aSorted);
     _ = b;
     //const b_sorted = bsort(b);
 
-    std.debug.print("a: {u}\n", .{a_sorted});
+    std.debug.print("a sorted: {any}\n", .{aSorted});
     // std.debug.print("b: {u}", .{b_sorted});
     return 5;
 }
 
-fn bsort(slc: []const u8) std.mem.Allocator.Error![]u8 {
-    const S = struct {
-        fn orderU8(_: void, lhs: u8, rhs: u8) bool {
-            return std.math.order(lhs, rhs) == .lt;
-        }
-    };
-
-    std.mem.sort(u8, slc, {}, S.orderU8);
-
-    return slc;
+/// Creates sorted copy of s.
+/// Caller owns the memory.
+fn sortCopy(malloc: Allocator, s: []const u8) Allocator.Error![]u8 {
+    const copy = try malloc.dupe(u8, s);
+    std.mem.sort(u8, copy, {}, std.sort.asc(u8));
+    return copy;
 }
