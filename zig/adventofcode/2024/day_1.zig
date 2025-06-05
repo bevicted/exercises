4e1ea2dfd0a7e46ba43be2be9b06e5ce286ec2e7
bevicted <170766675+bevicted@users.noreply.github.com>
2025-06-05 09:39:03 +0200

    holy cow this was painful
    Signed-off-by: bevicted <170766675+bevicted@users.noreply.github.com>

--- zig/adventofcode/2024/day_1.zig
+++ zig/adventofcode/2024/day_1.zig
@@ -1,16 +1,55 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 
+const inputFileName = "day_1_input.txt";
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}).init;
     defer {
         if (gpa.deinit() == .leak) @panic("memory leak detected");
     }
     const malloc = gpa.allocator();
-    var a = [_]u32{ 3, 4, 2, 1, 3, 3 };
-    var b = [_]u32{ 4, 3, 5, 3, 9, 3 };
-    const distance = try compareLists(malloc, &a, &b);
-    std.debug.print("dist: {}", .{distance});
+    const input = try getInput(malloc);
+    defer {
+        malloc.free(input.a);
+        malloc.free(input.b);
+    }
+
+    const distance = try compareLists(malloc, input.a, input.b);
+    std.debug.print("dist: {d}\n", .{distance});
+}
+
+/// Reads input from cwd and splits them into slices.
+fn getInput(malloc: Allocator) !struct { a: []u32, b: []u32 } {
+    // TODO: no idea how to get current file's dir right now
+    const file = try std.fs.cwd().openFile(inputFileName, .{});
+    defer file.close();
+    const reader = file.reader();
+
+    var a = std.ArrayList(u32).init(malloc);
+    defer a.deinit();
+    var b = std.ArrayList(u32).init(malloc);
+    defer b.deinit();
+
+    var isEven = true;
+    var buf: [1024]u8 = undefined;
+    while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        var it = std.mem.splitSequence(u8, line, "   ");
+        while (it.next()) |part| {
+            const n = try std.fmt.parseInt(u32, part, 10);
+            if (isEven) {
+                try a.append(n);
+                isEven = false;
+                continue;
+            }
+            try b.append(n);
+            isEven = true;
+        }
+    }
+    return .{
+        .a = try a.toOwnedSlice(),
+        .b = try b.toOwnedSlice(),
+    };
 }
 
 /// Creates sorted copies of the two slices and returns their distance.
