b9c2167451d7c57a0acb45e88f0accf563c59b83
bevicted <170766675+bevicted@users.noreply.github.com>
2025-10-17 13:06:55 +0200

    complete hw1c
    Signed-off-by: bevicted <170766675+bevicted@users.noreply.github.com>

--- uni/c/homework/hw1c.c
+++ uni/c/homework/hw1c.c
@@ -1,33 +1,30 @@
 #include <stdio.h>
 
-#define N 4
-#define C 10 // 4C2 + 4C3
+#define LEN 4
+#define Y "YES\n"
+#define N "NO\n"
 
 int main() {
-    int inputs[N], sums[C];
-    for (int i = 0; i < N; i++) {
+    int inputs[LEN], sum;
+    for (int i = 0; i < LEN; i++) {
         scanf("%d", &inputs[i]);
+        sum += inputs[i];
     }
 
-    int idx = 0;
-    for (int i = 0; i < N; i++) {
-        for (int j = i + 1; j < N; j++) {
-            sums[idx++] = inputs[i] + inputs[j];
-            for (int k = j + 1; k < N; k++) {
-                sums[idx++] = inputs[i] + inputs[j] + inputs[k];
-            }
+    for (int i = 0; i < LEN; i++) {
+        if (inputs[i] == sum - inputs[i]) {
+            printf(Y);
+            return 0;
         }
-    }
-
-    for (int i = 0; i < C; i++) {
-        for (int j = i + 1; j < C; j++) {
-            if (sums[i] == sums[j]) {
-                printf("IGEN\n");
+        for (int j = i + 1; j < LEN; j++) {
+            int two_sum = inputs[i] + inputs[j];
+            if (two_sum == sum - two_sum) {
+                printf(Y);
                 return 0;
             }
         }
     }
-    printf("NEM\n");
 
+    printf(N);
     return 0;
 }
